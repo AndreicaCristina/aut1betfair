9e7034c2b3eeae35107bca694ad97e722e02e426
Cristina Andreica <cristina.andreica@fanduel.com>
2024-01-27 00:20:05 +0200

    Merge remote-tracking branch 'origin/master'

--- src/test/java/courses/calculator/CalculatorTest.java
+++ src/test/java/courses/calculator/CalculatorTest.java
@@ -1,6 +1,10 @@
 package courses.calculator;
 
+//import jdk.jfr.Category;
 import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CalculatorTest {
     static Calculator c;
@@ -20,7 +24,7 @@ public class CalculatorTest {
     public void verifyAdditionComputeTest() {
         System.out.println("Verify addition works accordingly");
         Assert.assertEquals(5, c.compute(2, 3, "+"), 3);
-        Assert.assertEquals(5, c.compute(-2, 3, "+"), 3);
+//        Assert.assertEquals(5, c.compute(-2, 3, "+"), 3);
     }
 
     @Test
@@ -59,4 +63,47 @@ public class CalculatorTest {
         c = null;
     }
 
+    @Test
+    public void verifySubtractionComputeTest() {
+        System.out.println("Verify the subtraction work accordingly");
+    }
+
+    @Test
+    public void verifyDivisionComputeTest() {
+        System.out.println("Verify the division work accordingly  ");
+    }
+
+    @Test
+    public void verifyMultiplicationComputeTest() {
+        System.out.println("Verify the multiplication work accordingly ");
+    }
+    @Test
+    public void verifyDivisionByZero() {
+       System.out.println("Verify the division by zero work accordingly ");
+    }
+
+    @Test
+    public void testEquality(){
+        String str1 = "Hello";
+        String str2 = "Hello";
+        Assert.assertEquals(str1, str2);
+        System.out.println("The strings are equal");
+    }
+
+    @Test
+    public void testNull() {
+        Object obj1 = null;
+        Assert.assertNull(null);
+        System.out.println("The object is null");
+    }
+
+    @Test
+    public void testList() {
+        List<String> list = new ArrayList<>();
+        list.add("Test1");
+        list.add("Test2");
+        Assert.assertEquals(2, list.size());
+        Assert.assertTrue(list.contains("Test1"));
+        System.out.println("The list contains 2 elements, and one of them includes the string Test1");
+    }
 }
